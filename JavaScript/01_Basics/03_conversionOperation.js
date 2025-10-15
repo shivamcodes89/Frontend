8d35aa344775754e792f7a1e86b5c67ce413a01d
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-15 06:36:38 +0530

    Operation

--- JavaScript/01_Basics/03_conversionOperation.js
+++ JavaScript/01_Basics/03_conversionOperation.js
@@ -81,3 +81,31 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ******************** Operation ********************** 
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);  // -3
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(5/2);
+console.log(3%2);
+
+
+let str1 = "Hello, ";
+let str2 = "Shivam";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("1" + 2);  // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 + "2");  // 32
+
